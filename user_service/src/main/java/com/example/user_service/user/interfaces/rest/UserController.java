87630955fbb0108c261040b57283033e755654f1
Leydi-Aro <89089568+Leydi-Aro@users.noreply.github.com>
2024-06-25 22:07:21 -0500

    user test

--- user_service/src/main/java/com/example/user_service/user/interfaces/rest/UserController.java
+++ user_service/src/main/java/com/example/user_service/user/interfaces/rest/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping(value = "/api/v1/users", produces = MediaType.APPLICATION_JSON_VALUE)
 @Validated
